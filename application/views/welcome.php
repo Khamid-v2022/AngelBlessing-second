7ebc92f15d23ed399ee124d6dddaa4bb990b43cb
Khamid-v2022 <khamid.webdev@gmail.com>
2022-11-07 13:21:42 +0800

    fix responsive manubar

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,30 +1,29 @@
 <body>
     <section class="menu-section">
         <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-light">
-              <a class="navbar-brand" href="https://www.askangelblessing.com">
-                  <img width="125" height="125" src="<?=base_url()?>assets/img/logo.png" alt="">
-              </a>
-              <div class="menu-item-wrapper">
-                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                      <li class="nav-item active">
-                        <a class="nav-link" href="https://www.askangelblessing.com/">Home</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="https://www.askangelblessing.com/privacy-policy/">Privacy Policy</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="https://www.askangelblessing.com/about-us/">About Us</a>
-                      </li>
-                    </ul>
-                    
-                  </div>
-              </div>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <a class="navbar-brand" href="https://www.askangelblessing.com">
+                    <img width="125" height="125" src="<?=base_url()?>assets/img/logo.png" alt="">
+                </a>
+               
+                <div class="menu-item-wrapper">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="https://www.askangelblessing.com/">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.askangelblessing.com/privacy-policy/">Privacy Policy</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.askangelblessing.com/about-us/">About Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </div>
     </section>
@@ -50,6 +49,7 @@
             </div>
         </div>
     </section>
+
     <section class="main">
         <div class="container">  
             <div class="google-ads-place">
@@ -73,7 +73,7 @@
                         <a type="button" class="color-btn" img-color="Red">
                             <div class="flip-container flip-box-inner">
                                 <div class="flip-box-front">
-                                    <img src="<?=base_url()?>assets/img/card/1.png" alt="1" title="1" class="">
+                                    <img src="<?=base_url()?>assets/img/card/1.png" alt="Red" title="Red">
                                 </div>
                             </div>
                         </a>
@@ -82,7 +82,7 @@
                         <a type="button" class="color-btn" img-color="Blue">
                             <div class="flip-container flip-box-inner">
                                 <div class="flip-box-front">
-                                    <img src="<?=base_url()?>assets/img/card/2.png" alt="2" title="2" class="">
+                                    <img src="<?=base_url()?>assets/img/card/2.png" alt="Blue" title="Blue">
                                 </div>
                             </div>
                         </a>
@@ -91,7 +91,7 @@
                        <a type="button" class="color-btn" img-color="Green">
                             <div class="flip-container flip-box-inner">
                                 <div class="flip-box-front">
-                                    <img src="<?=base_url()?>assets/img/card/3.png" alt="3" title="3" class="">
+                                    <img src="<?=base_url()?>assets/img/card/3.png" alt="Green" title="Green">
                                 </div>
                             </div>
                         </a>
@@ -100,7 +100,7 @@
                         <a type="button" class="color-btn" img-color="Yellow">
                             <div class="flip-container flip-box-inner">
                                 <div class="flip-box-front">
-                                    <img src="<?=base_url()?>assets/img/card/4.png" alt="4" title="4" class="">
+                                    <img src="<?=base_url()?>assets/img/card/4.png" alt="Yellow" title="Yellow">
                                 </div>
                             </div>
                         </a>
@@ -109,7 +109,7 @@
                         <a type="button" class="color-btn" img-color="Pink">
                             <div class="flip-container flip-box-inner">
                                 <div class="flip-box-front">
-                                    <img src="<?=base_url()?>assets/img/card/5.png" alt="5" title="5" class="">
+                                    <img src="<?=base_url()?>assets/img/card/5.png" alt="5" title="5">
                                 </div>
                             </div>
                         </a>
@@ -118,7 +118,7 @@
                         <a type="button" class="color-btn" img-color="Grey">
                             <div class="flip-container flip-box-inner">
                                 <div class="flip-box-front">
-                                    <img src="<?=base_url()?>assets/img/card/6.png" alt="6" title="6" class="">
+                                    <img src="<?=base_url()?>assets/img/card/6.png" alt="Grey" title="Grey">
                                 </div>
                             </div>
                         </a>
@@ -127,7 +127,7 @@
                         <a type="button" class="color-btn" img-color="Orange">
                             <div class="flip-container flip-box-inner">
                                 <div class="flip-box-front">
-                                    <img src="<?=base_url()?>assets/img/card/7.png" alt="7" title="7" class="">
+                                    <img src="<?=base_url()?>assets/img/card/7.png" alt="Orange" title="Orange">
                                 </div>
                             </div>
                         </a>
@@ -136,7 +136,7 @@
                         <a type="button" class="color-btn" img-color="Brown">
                             <div class="flip-container flip-box-inner">
                                 <div class="flip-box-front">
-                                    <img src="<?=base_url()?>assets/img/card/8.png" alt="8" title="8" class="">
+                                    <img src="<?=base_url()?>assets/img/card/8.png" alt="Brown" title="Brown">
                                 </div>
                             </div>
                         </a>
@@ -145,7 +145,7 @@
                        <a type="button" class="color-btn" img-color="White">
                             <div class="flip-container flip-box-inner">
                                 <div class="flip-box-front">
-                                    <img src="<?=base_url()?>assets/img/card/9.png" alt="9" title="9" class="">
+                                    <img src="<?=base_url()?>assets/img/card/9.png" alt="White" title="White">
                                 </div>
                             </div>
                         </a>
