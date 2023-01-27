c31d405bdf72cfaee2d09b1770fbd738f7dda813
Khamid-v2022 <khamid.webdev@gmail.com>
2023-01-27 16:31:37 +0800

    add a block that go to some link to horoscope page

--- application/views/horoscope_main.php
+++ application/views/horoscope_main.php
@@ -46,8 +46,9 @@
                 <script>
                      (adsbygoogle = window.adsbygoogle || []).push({});
                 </script>
-            </div>          
-            <div class="horoscope-wrapper">
+            </div>   
+
+            <div class="horoscope-wrapper mt-3">
                 <h3 class="mb-5">HOROSCOPE READING</h3>
                 <div class="horoscopes text-center">
                     <a type="button" href="#details" class="horoscope-btn active" data-name="Aries">
@@ -123,7 +124,11 @@
                     </div>
                 </div>
             </div>
-
+            <div class="text-center mt-5 mb-3">
+                <h2>Looking To Add Some Extra Luck Into Your Day?</h2>
+                <h6 class="mt-3">Click On The Button To Discover The Little-Known "Salty Path" To Abundance</h6>
+                <a type="button" class="btn btn-outline-danger mt-3 font-weight-bold" href="https://www.askangelblessing.com/moreluck" target="_blank">Discover The More Salt More Luck Journey</a>
+            </div>
             <div class="google-ads-place">
                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6130278165783644"
                      crossorigin="anonymous"></script>
